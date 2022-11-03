e4afb9eccd0989a369745ea757722c86515c2e9b
toan1231 <100801887+toan1231@users.noreply.github.com>
2022-11-03 07:16:43 -0400

    Update Hellow.java

--- src/test/java/Hellow.java
+++ src/test/java/Hellow.java
@@ -1,20 +1,5 @@
 public class Hellow {
-    public static void main(String[] args) {
-        int a = 123456;
-        System.out.println(method(a));
-
-    }
-
-    public static int method(int a) {
-        String reverse = "";
-        int b = 1;
-        String string = String.valueOf(a);
-        for (int i = string.length() - 1; i >= 0; i--) {
-            reverse += string.charAt(i);
-            b = Integer.valueOf(reverse);
-
-        }
-        return b;
+System.out.prinln("my name is john")
 
     }
 }
